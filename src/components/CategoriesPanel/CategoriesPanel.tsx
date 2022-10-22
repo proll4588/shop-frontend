0f7844378771e9e8cc4bedfe5eaf2d40654186d9
proll4588 <prol4588@gmail.com>
2022-10-22 16:14:52 +0700

    refactor CategoriesPanel

--- src/components/CategoriesPanel/CategoriesPanel.tsx
+++ src/components/CategoriesPanel/CategoriesPanel.tsx
@@ -1,14 +1,78 @@
+/* Хуки */
 import React, { FC, useState } from 'react'
+
+/* Описание компонента */
 import styles from './CategoriesPanel.module.scss'
 import CategoriesPanelProps from './CategoriesPanel.props'
 
+/* Иконки */
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 import { AiOutlineUnorderedList } from 'react-icons/ai'
 
+/* Компоненты */
 import TypesList from '../TypesList/TypesList'
 
+/* Бибилиотеки */
 import classNames from 'classnames'
+import { IGlobalGoodsTypes } from '../../interfaces/goodsTypes.interface'
 
+// Заголовок панели
+interface HeadProps {
+    onClick?: () => void
+}
+const Head: FC<HeadProps> = ({ onClick }) => {
+    return (
+        <div
+            className={styles.CategoriesPanel__title}
+            onClick={onClick}
+        >
+            <AiOutlineUnorderedList
+                className={styles.CategoriesPanel__listIcon}
+            />
+
+            <div className={styles.CategoriesPanel__titleName}>Категории</div>
+        </div>
+    )
+}
+
+// Список глобальных типов
+interface ListProps {
+    globalGoodsTypes: IGlobalGoodsTypes[]
+    selected: number
+    inHandler?: (id: number) => void
+}
+const List: FC<ListProps> = ({ globalGoodsTypes, selected, inHandler }) => {
+    return (
+        <ul className={styles.CategoriesPanel__listContainer}>
+            {globalGoodsTypes.map((type) => (
+                <li
+                    className={styles.CategoriesPanel__typeContainer}
+                    key={type.id}
+                    onMouseEnter={() => {
+                        inHandler(type.id)
+                    }}
+                >
+                    <div
+                        className={classNames(
+                            styles.CategoriesPanel__typeName,
+                            selected === type.id
+                                ? styles.CategoriesPanel__typeName_selected
+                                : ''
+                        )}
+                    >
+                        {type.name}
+                    </div>
+
+                    <MdOutlineKeyboardArrowRight
+                        className={styles.CategoriesPanel__arrowIcon}
+                    />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+// Панель глобальных типов
 const CategoriesPanel: FC<CategoriesPanelProps> = ({
     globalGoodsTypes,
     startSelect,
@@ -16,19 +80,23 @@ const CategoriesPanel: FC<CategoriesPanelProps> = ({
     className,
     ...props
 }) => {
+    // Состояние открытости панели
     const [isOpen, setIsOpen] = useState<boolean>(lock)
+
+    // id выбранного глобального типа
     const [selected, setSelected] = useState<number>(
         startSelect !== undefined ? globalGoodsTypes[0].id : null
     )
 
+    // Функция переключения открытости
     const openCloseHandler = () => {
-        if (lock) {
-            return
-        }
+        if (lock) return
+
         setSelected(null)
         setIsOpen((prev) => !prev)
     }
 
+    // Функция выбора глобального типа
     const inHandler = (id) => {
         setSelected(id)
     }
@@ -38,63 +106,24 @@ const CategoriesPanel: FC<CategoriesPanelProps> = ({
             className={classNames(styles.CategoriesPanel, className)}
             {...props}
         >
-            <div className={styles.CategoriesPanel__container}>
-                <div
-                    className={styles.CategoriesPanel__title}
-                    onClick={openCloseHandler}
-                >
-                    <AiOutlineUnorderedList
-                        className={styles.CategoriesPanel__listIcon}
+            <Head onClick={openCloseHandler} />
+            {isOpen && (
+                <>
+                    <List
+                        globalGoodsTypes={globalGoodsTypes}
+                        selected={selected}
+                        inHandler={inHandler}
                     />
-
-                    <div className={styles.CategoriesPanel__titleName}>
-                        Категории
-                    </div>
-                </div>
-
-                {isOpen && (
-                    <ul className={styles.CategoriesPanel__listContainer}>
-                        {globalGoodsTypes.map((type) => (
-                            <li
-                                className={
-                                    styles.CategoriesPanel__typeContainer
-                                }
-                                key={type.id}
-                                onMouseEnter={() => {
-                                    inHandler(type.id)
-                                }}
-                            >
-                                <div
-                                    className={`${
-                                        styles.CategoriesPanel__typeName
-                                    } ${
-                                        selected === type.id
-                                            ? styles.CategoriesPanel__typeName_selected
-                                            : ''
-                                    }`}
-                                >
-                                    {type.name}
-                                </div>
-
-                                <MdOutlineKeyboardArrowRight
-                                    className={
-                                        styles.CategoriesPanel__arrowIcon
-                                    }
-                                />
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-            {selected !== null ? (
-                <TypesList
-                    localGoodsTypes={
-                        globalGoodsTypes.find((el) => el.id === selected)
-                            .local_type_goods
-                    }
-                />
-            ) : (
-                ''
+                    {selected !== null && (
+                        <TypesList
+                            localGoodsTypes={
+                                globalGoodsTypes.find(
+                                    (el) => el.id === selected
+                                ).local_type_goods
+                            }
+                        />
+                    )}
+                </>
             )}
         </div>
     )
