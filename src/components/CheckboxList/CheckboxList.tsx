b53bc7e77e1c18c828a8b53081b0fa10910de6fa
proll4588 <prol4588@gmail.com>
2022-10-22 16:44:26 +0700

    refactor CheckboxList

--- src/components/CheckboxList/CheckboxList.tsx
+++ src/components/CheckboxList/CheckboxList.tsx
@@ -1,13 +1,20 @@
+/* Хуки */
 import React, { FC, useEffect, useState } from 'react'
+
+/* Компоненты */
 import Checkbox from '../UI/Checkbox/Checkbox'
+
+/* Описание компонента */
 import styles from './CheckboxList.module.scss'
 import CheckboxListProps from './CheckboxList.props'
 
 const CheckboxList: FC<CheckboxListProps> = ({ data, onChange }) => {
+    // Состояние списка
     const [states, setStates] = useState(
         data.map((el) => ({ id: el.id, state: false }))
     )
 
+    // При изменении состояния списка вызываем функцию onChange
     useEffect(() => {
         onChange && onChange(states)
     }, [states])
@@ -15,25 +22,27 @@ const CheckboxList: FC<CheckboxListProps> = ({ data, onChange }) => {
     return (
         <div className={styles.CheckboxList}>
             <div className={styles.CheckboxList__container}>
-                {data.map((el) => (
-                    <Checkbox
-                        lable={el.name}
-                        key={el.id}
-                        state={states.find((e) => e.id === el.id).state}
-                        onChange={(state) => {
-                            setStates((prev) => {
-                                let newState = prev.map((pr) => {
-                                    if (pr.id === el.id) {
-                                        return { id: pr.id, state: !pr.state }
-                                    }
-                                    return pr
-                                })
-
-                                return newState
+                {data.map((el) => {
+                    const changeHandler = (state) => {
+                        setStates((prev) =>
+                            prev.map((pr) => {
+                                if (pr.id === el.id) {
+                                    return { id: pr.id, state: state }
+                                }
+                                return pr
                             })
-                        }}
-                    />
-                ))}
+                        )
+                    }
+
+                    return (
+                        <Checkbox
+                            lable={el.name}
+                            key={el.id}
+                            state={states.find((e) => e.id === el.id).state}
+                            onChange={changeHandler}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
