3882ab08ade9ab86a1b25c65df7a0250830453e0
proll4588 <prol4588@gmail.com>
2022-10-28 22:05:27 +0700

    add keyFields for apollo cache

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
     uri: 'http://192.168.0.42:4000/',
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        typePolicies: { FiltersListDataElement: { keyFields: ['value'] } },
+    }),
 })
 
 export default client
