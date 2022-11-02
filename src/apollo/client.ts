2ca4285da779366558480241d506920e0d293cf9
proll4588 <prol4588@gmail.com>
2022-11-02 21:52:15 +0700

    add auth with token

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+    ApolloClient,
+    ApolloLink,
+    concat,
+    HttpLink,
+    InMemoryCache,
+} from '@apollo/client'
+
+const httpLink = new HttpLink({ uri: 'http://192.168.0.42:4000/' })
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+    const token = localStorage.getItem('token')
+    operation.setContext({
+        headers: {
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    })
+    return forward(operation)
+})
 
 const client = new ApolloClient({
-    uri: 'http://192.168.0.42:4000/',
+    link: concat(authMiddleware, httpLink),
     cache: new InMemoryCache({
         typePolicies: { FiltersListDataElement: { keyFields: ['value'] } },
     }),
