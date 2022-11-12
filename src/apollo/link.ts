f82a7a41f310fe8d0027ea6c45a4b258549997de
proll4588 <prol4588@gmail.com>
2022-11-12 12:41:42 +0700

    add upload link

--- src/apollo/link.ts
+++ src/apollo/link.ts
@@ -1,17 +1,22 @@
 import { ApolloLink, concat, HttpLink } from '@apollo/client'
+import { createUploadLink } from 'apollo-upload-client'
 
 export const getLink = () => {
-  const httpLink = new HttpLink({ uri: 'http://192.168.0.42:4000/' })
-
-  const authMiddleware = new ApolloLink((operation, forward) => {
-    const token = localStorage.getItem('token')
-    operation.setContext({
-      headers: {
-        authorization: token && token !== 'null' ? `${token}` : '',
-      },
+    // const httpLink = new HttpLink({ uri: 'http://192.168.0.42:4000/graphql' })
+    const httpLink = new createUploadLink({
+        uri: 'http://192.168.0.42:4000/graphql',
     })
-    return forward(operation)
-  })
 
-  return concat(authMiddleware, httpLink)
+    const authMiddleware = new ApolloLink((operation, forward) => {
+        const token = localStorage.getItem('token')
+        operation.setContext({
+            headers: {
+                authorization: token && token !== 'null' ? `${token}` : '',
+                'apollo-require-preflight': true,
+            },
+        })
+        return forward(operation)
+    })
+
+    return concat(authMiddleware, httpLink)
 }
