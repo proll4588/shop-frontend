6b36d2c3097f4abeed675974837773597abb01a8
proll4588 <prol4588@gmail.com>
2022-11-19 12:11:10 +0700

    refactor

--- src/hooks/cart.hook.ts
+++ src/hooks/cart.hook.ts
@@ -1,31 +1,27 @@
-import { useLayoutEffect } from 'react'
 import {
     GET_CART,
     ADD_TO_CART,
     REMOVE_FROM_CART,
     CHANGE_CART,
 } from './../apollo/fetchs'
-import { useApolloClient, useMutation, useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { useSetRecoilState } from 'recoil'
 import countsAtom from '../atoms/counts.atom'
 
 const useCart = () => {
-    const client = useApolloClient()
     const setCounts = useSetRecoilState(countsAtom)
 
     const { data, error, loading } = useQuery(GET_CART)
 
-    const [add, addInfo] = useMutation(ADD_TO_CART)
-    const [remove, remInfo] = useMutation(REMOVE_FROM_CART)
-    const [change, changeInfo] = useMutation(CHANGE_CART)
-
-    useLayoutEffect(() => {
-        if (addInfo.data || remInfo.data || changeInfo.data) {
-            client.refetchQueries({
-                include: [GET_CART],
-            })
-        }
-    }, [addInfo.data, remInfo.data, changeInfo.data])
+    const [add, addInfo] = useMutation(ADD_TO_CART, {
+        refetchQueries: [{ query: GET_CART }],
+    })
+    const [remove, remInfo] = useMutation(REMOVE_FROM_CART, {
+        refetchQueries: [{ query: GET_CART }],
+    })
+    const [change, changeInfo] = useMutation(CHANGE_CART, {
+        refetchQueries: [{ query: GET_CART }],
+    })
 
     const addToCart = (goodId, count) => {
         setCounts((prev) => ({ ...prev, cart: prev.cart + 1 }))
--- src/hooks/favorite.hook.ts
+++ src/hooks/favorite.hook.ts
@@ -1,35 +1,26 @@
 import { useSetRecoilState } from 'recoil'
-import { useLayoutEffect } from 'react'
 import {
     GET_FAVORITE,
     ADD_TO_FAVORITE,
     REMOVE_FROM_FAVORITE,
 } from './../apollo/fetchs'
-import { useQuery, useMutation, useApolloClient } from '@apollo/client'
+import { useQuery, useMutation } from '@apollo/client'
 import countsAtom from '../atoms/counts.atom'
 
 const useFavorite = () => {
-    const client = useApolloClient()
     const setCounts = useSetRecoilState(countsAtom)
 
     /* Запросы на товары в избранном */
     const { data, error, loading } = useQuery(GET_FAVORITE)
-    const [add, addInfo] = useMutation(ADD_TO_FAVORITE)
-    const [remove, remInfo] = useMutation(REMOVE_FROM_FAVORITE)
+    const [add, addInfo] = useMutation(ADD_TO_FAVORITE, {
+        refetchQueries: [{ query: GET_FAVORITE }],
+    })
+    const [remove, remInfo] = useMutation(REMOVE_FROM_FAVORITE, {
+        refetchQueries: [{ query: GET_FAVORITE }],
+    })
     /* =========================================================== */
 
-    // Перезапрос избранных товаров при добавления нового или удаления уже имеющего
-    useLayoutEffect(() => {
-        if (addInfo.data || remInfo.data) {
-            client.refetchQueries({
-                include: [GET_FAVORITE],
-            })
-        }
-    }, [addInfo.data, remInfo.data])
-
     const addToFavorite = (goodId) => {
-        // console.log('add')
-
         setCounts((prev) => ({ ...prev, favorite: prev.favorite + 1 }))
         add({
             variables: {
