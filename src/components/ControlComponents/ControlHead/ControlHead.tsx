eddaf5a8f6aa9d079c8a2ca9474c16d415fbdb8e
proll4588 <prol4588@gmail.com>
2022-12-21 00:11:17 +0700

    refactor control head

--- src/components/ControlComponents/ControlHead/ControlHead.tsx
+++ src/components/ControlComponents/ControlHead/ControlHead.tsx
@@ -1,16 +1,65 @@
-import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import React, { FC, useState } from 'react'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import Button from '../../UI/Button/Button'
 import styles from './ControlHead.module.scss'
 import ControlHeadProps from './ControlHead.props'
 
+const menu = [
+    { id: 1, name: 'Редактирование товаров', route: 'goodsRedactor', w: 210 },
+    { id: 2, name: 'Статистика', route: 'statistics', w: 110 },
+    { id: 3, name: 'Работа с заказами', route: 'orders', w: 150 },
+    // { id: 4, name: '', route: '' },
+    // { id: 5, name: '', route: '' },
+]
+
 const ControlHead: FC<ControlHeadProps> = () => {
+    // const location = useLocation().pathname.substring(1)
+
+    // const getW = () => {
+    //     return menu.find((el) => el.route === location).w + 10
+    // }
+
+    // const getLeft = () => {
+    //     let sum = 10
+    //     const selected = menu.find((el) => el.route === location).id
+
+    //     menu.filter((el) => el.id < selected).forEach(
+    //         (el) => (sum += el.w + 10)
+    //     )
+
+    //     return sum
+    // }
+
     return (
         <div className={styles.ControlHead}>
             <div className={styles.ControlHead__container}>
-                <Link to={'/goodsRedactor'}>
-                    <Button large>Редактирование товаров</Button>
-                </Link>
+                <ul className={styles.ControlHead__list}>
+                    {menu.map((el) => (
+                        <li key={el.id}>
+                            <NavLink
+                                to={'/' + el.route}
+                                className={(isActive) => {
+                                    return classNames(
+                                        styles.ControlHead__element,
+                                        isActive
+                                            ? styles.ControlHead__element_active
+                                            : ''
+                                    )
+                                }}
+                            >
+                                {el.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+                {/* <div
+                    className={styles.ControlHead__selector}
+                    // style={{
+                    //     width: getW(),
+                    //     left: getLeft(),
+                    // }}
+                /> */}
             </div>
         </div>
     )
