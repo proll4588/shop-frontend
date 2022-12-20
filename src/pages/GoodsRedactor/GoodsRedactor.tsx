bca58a918614fb89eb0e0ea2007ae20244edb1c7
proll4588 <prol4588@gmail.com>
2022-12-21 00:10:11 +0700

    refactor goods redactor page

--- src/pages/GoodsRedactor/GoodsRedactor.tsx
+++ src/pages/GoodsRedactor/GoodsRedactor.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from '@apollo/client'
 import React, { FC, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { GET_GOODS } from '../../apollo/fetchs'
 import ControlGoodsList from '../../components/ControlComponents/ControlGoodsList/ControlGoodsList'
 import Input from '../../components/UI/Input/Input'
 import styles from './GoodsRedactor.module.scss'
 import GoodsRedactorProps from './GoodsRedactor.props'
+
+const AddElement = () => {
+    return <div className={styles.AddElement}> Добавить новый товар </div>
+}
 
 const GoodsRedactor: FC<GoodsRedactorProps> = () => {
     const [search, setSearch] = useState<string | null>('')
@@ -16,22 +19,27 @@ const GoodsRedactor: FC<GoodsRedactorProps> = () => {
     })
 
     const searchHandler = (e) => {
-        setSearch(e.target.value)
+        setSearch(e)
     }
+
+    if (data) console.log(data)
 
     return (
         <div className={styles.GoodsRedactor}>
             <div className={styles.GoodsRedactor__container}>
+                {/* TODO:Сделать задержку поиска */}
                 <Input
                     onChange={searchHandler}
+                    placeholder='Поиск по названию или id товара'
                     value={search}
                     type='text'
                 />
-                {!loading && !error ? (
-                    <ControlGoodsList data={data.goods} />
+                {!loading && !error && !!data.getGoods.length ? (
+                    <ControlGoodsList data={data.getGoods} />
                 ) : (
                     ''
                 )}
+                <AddElement />
             </div>
         </div>
     )
