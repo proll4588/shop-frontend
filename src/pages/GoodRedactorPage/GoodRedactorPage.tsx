d1b3c3ae4f55b87d50cfe008fc6ca33df452dfa4
proll4588 <prol4588@gmail.com>
2022-12-22 00:52:50 +0700

    add adding new brands & updating good data

--- src/pages/GoodRedactorPage/GoodRedactorPage.tsx
+++ src/pages/GoodRedactorPage/GoodRedactorPage.tsx
@@ -1,21 +1,18 @@
 import { useMutation, useQuery } from '@apollo/client'
-import React, {
-    FC,
-    HTMLAttributes,
-    HTMLInputTypeAttribute,
-    useEffect,
-    useState,
-} from 'react'
+import React, { FC, Fragment, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {
     ADD_GOOD_PHOTO,
+    CREATE_BRAND,
     GET_BRANDS,
     GET_GOOD,
     GOODS_PATH,
     REMOVE_GOOD_PHOTO,
+    SEARCH_TYPE,
     SET_MAIN_GOOD_PHOTO,
+    UPDATE_GOOD_DATA,
+    UPLOAD_BRAND_LOGO,
 } from '../../apollo/fetchs'
-import InputGoodsParams from '../../components/ControlComponents/InputGoodsParams/InputGoodsParams'
 import styles from './GoodRedactorPage.module.scss'
 import GoodRedactorPageProps from './GoodRedactorPage.props'
 import Dropzone from 'react-dropzone'
@@ -25,6 +22,8 @@ import { BiTrashAlt } from 'react-icons/bi'
 import { Controller, useForm } from 'react-hook-form'
 import { LabelInput } from '../../components/LabelInput'
 import MyCombobox from '../../components/UI/MyCombobox/MyCombobox'
+import Button from '../../components/UI/Button/Button'
+import { Dialog, Transition } from '@headlessui/react'
 
 interface OtherPhotoProps {
     photo: {
@@ -207,6 +206,138 @@ const Photo: FC<PhotoProps> = ({ mainPhoto, otherPhoto }) => {
     )
 }
 
+interface LogoUploaderProps {
+    onDrop?: (file: any) => void
+    isDroped?: boolean
+}
+const LogoUploader: FC<LogoUploaderProps> = ({ onDrop, isDroped }) => {
+    return (
+        <Dropzone
+            onDrop={([file]) => onDrop(file)}
+            accept={{
+                'image/jpeg': [],
+                'image/png': [],
+            }}
+        >
+            {({ getRootProps, getInputProps, isDragActive }) => (
+                <div
+                    {...getRootProps({
+                        className: styles.OtherPhotoDrag,
+                    })}
+                >
+                    <input {...getInputProps()} />
+                    <div
+                        className={classNames(
+                            styles.OtherPhotoDrag__container,
+                            isDragActive
+                                ? styles.OtherPhotoDrag__container_active
+                                : ''
+                        )}
+                    >
+                        {isDroped ? <>ok</> : <>+</>}
+                    </div>
+                </div>
+            )}
+        </Dropzone>
+    )
+}
+
+const BrandCreatorBtn = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const open = () => setIsOpen(true)
+    const close = () => setIsOpen(false)
+
+    return (
+        <>
+            <Button
+                secondary
+                onClick={open}
+            >
+                +
+            </Button>
+
+            {isOpen && (
+                <BrandCreaterPopup
+                    close={close}
+                    isOpen={isOpen}
+                />
+            )}
+        </>
+    )
+}
+
+interface BrandCreaterPopupProps {
+    close: () => void
+    isOpen: boolean
+}
+const BrandCreaterPopup: FC<BrandCreaterPopupProps> = ({ close, isOpen }) => {
+    const [name, setName] = useState('')
+
+    const [uploadLogo, uploadData] = useMutation(UPLOAD_BRAND_LOGO)
+    const [createBrand, createData] = useMutation(CREATE_BRAND)
+
+    const dropLogo = (file) => uploadLogo({ variables: { file } })
+    const create = () => {
+        if (name.length)
+            createBrand({
+                variables: {
+                    name,
+                    logo:
+                        uploadData.data && uploadData.data.uploadLogoForNewBrand
+                            ? uploadData.data.uploadLogoForNewBrand
+                            : undefined,
+                },
+            })
+    }
+
+    useEffect(() => {
+        if (createData.data) close()
+    }, [createData])
+
+    return (
+        <Dialog
+            onClose={close}
+            open={isOpen}
+            as='div'
+            className={styles.BrandCreaterPopup__dialog}
+        >
+            {/* <Transition.Child as={Fragment}> */}
+            <div className={styles.BrandCreaterPopup__back} />
+            {/* </Transition.Child> */}
+
+            <div className={styles.BrandCreaterPopup__dialogContent}>
+                {/* <Transition.Child as={Fragment}> */}
+                <Dialog.Panel className={styles.BrandCreaterPopup__dialogPanel}>
+                    <Dialog.Title
+                        as='h4'
+                        className={styles.BrandCreaterPopup__dialogTitle}
+                    >
+                        Создание нового производителя
+                    </Dialog.Title>
+
+                    <LabelInput label='Имя производителя'>
+                        <input
+                            type='text'
+                            placeholder='Имя производителя'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </LabelInput>
+
+                    <LogoUploader
+                        isDroped={uploadData.data ? true : false}
+                        onDrop={dropLogo}
+                    />
+
+                    <Button onClick={create}>Создать</Button>
+                </Dialog.Panel>
+                {/* </Transition.Child> */}
+            </div>
+        </Dialog>
+    )
+}
+
 interface BrandsComboboxProps {
     defaultValue?: {
         id: number
@@ -226,13 +357,49 @@ const BrandsCombobox: FC<BrandsComboboxProps> = ({
 
     const brands = data ? data.getBrands : []
     return (
-        <MyCombobox
-            elements={brands}
-            onQuering={setQuery}
-            onSelect={onChange}
-            defaultValue={defaultValue}
-            loading={loading || !!error}
-        />
+        <div className={styles.BrandsCombobox}>
+            <MyCombobox
+                elements={brands}
+                onQuering={setQuery}
+                onSelect={onChange}
+                defaultValue={defaultValue}
+                loading={loading || !!error}
+            />
+            {/* <BrandCreaterPopup /> */}
+            <BrandCreatorBtn />
+        </div>
+    )
+}
+
+interface SubTypesComboboxProps {
+    defaultValue?: {
+        id: number
+        name: string
+    }
+    onChange?: (id: number) => void
+}
+const SubTypesCombobox: FC<SubTypesComboboxProps> = ({
+    defaultValue,
+    onChange,
+}) => {
+    const [query, setQuery] = useState('')
+
+    const { data, error, loading } = useQuery(SEARCH_TYPE, {
+        variables: { search: query },
+    })
+
+    const types = data ? data.getGoodTypesBySearch : []
+
+    return (
+        <div className={styles.BrandsCombobox}>
+            <MyCombobox
+                elements={types}
+                onQuering={setQuery}
+                onSelect={onChange}
+                defaultValue={defaultValue}
+                loading={loading || !!error}
+            />
+        </div>
     )
 }
 
@@ -240,13 +407,39 @@ interface DescriptionProps {
     good: IGood
 }
 const Description: FC<DescriptionProps> = ({ good }) => {
+    const { id } = useParams()
     const {
         register,
         handleSubmit,
         control,
         formState: { errors },
     } = useForm()
-    const onSubmit = (data) => console.log(data)
+    const [update, updateData] = useMutation(UPDATE_GOOD_DATA, {
+        refetchQueries: [
+            { query: GET_GOOD, variables: { goodId: Number(id) } },
+        ],
+    })
+
+    const onSubmit = (data) => {
+        update({
+            variables: {
+                goodId: Number(id),
+                name: data.name === good.name ? undefined : data.name,
+                subTypeId:
+                    data.subTypeId === good.sub_type_goods.id
+                        ? undefined
+                        : Number(data.subTypeId),
+                brandId:
+                    data.brandId === good.brands.id
+                        ? undefined
+                        : Number(data.brandId),
+                description:
+                    data.description === good.description
+                        ? undefined
+                        : data.description,
+            },
+        })
+    }
     console.log(errors)
 
     return (
@@ -256,38 +449,60 @@ const Description: FC<DescriptionProps> = ({ good }) => {
                     onSubmit={handleSubmit(onSubmit)}
                     className={styles.Description__form}
                 >
-                    <LabelInput label='Наименование товара'>
-                        <input
-                            type='text'
-                            placeholder='Наименование товара'
-                            defaultValue={good.name}
-                            {...register('name', { required: true })}
-                        />
-                    </LabelInput>
-
-                    <LabelInput label='Производитель'>
-                        <Controller
-                            name='brandId'
-                            control={control}
-                            render={({ field }) => (
-                                <BrandsCombobox
-                                    defaultValue={good.brands}
-                                    onChange={field.onChange}
+                    <div className={styles.Description__formContainer}>
+                        <div className={styles.Description__left}>
+                            <LabelInput label='Наименование товара'>
+                                <input
+                                    type='text'
+                                    placeholder='Наименование товара'
+                                    defaultValue={good.name}
+                                    {...register('name', { required: true })}
                                 />
-                            )}
-                            defaultValue={good.brands.id}
-                        />
-                    </LabelInput>
+                            </LabelInput>
 
-                    <LabelInput label='Описание товара'>
-                        <textarea
-                            defaultValue={good.description}
-                            className={styles.Description__textArea}
-                            {...register('description', { required: false })}
-                        />
-                    </LabelInput>
+                            <LabelInput label='Тип товара'>
+                                <Controller
+                                    name='subTypeId'
+                                    control={control}
+                                    render={({ field }) => (
+                                        <SubTypesCombobox
+                                            defaultValue={good.sub_type_goods}
+                                            onChange={field.onChange}
+                                        />
+                                    )}
+                                    defaultValue={good.brands.id}
+                                />
+                            </LabelInput>
 
-                    <div className={styles.Description__priceContainer}>
+                            <LabelInput label='Производитель'>
+                                <Controller
+                                    name='brandId'
+                                    control={control}
+                                    render={({ field }) => (
+                                        <BrandsCombobox
+                                            defaultValue={good.brands}
+                                            onChange={field.onChange}
+                                        />
+                                    )}
+                                    defaultValue={good.brands.id}
+                                />
+                            </LabelInput>
+                        </div>
+                        <div className={styles.Description__right}>
+                            <LabelInput label='Описание товара'>
+                                <textarea
+                                    defaultValue={good.description}
+                                    className={styles.Description__textArea}
+                                    {...register('description', {
+                                        required: false,
+                                    })}
+                                />
+                            </LabelInput>
+                        </div>
+                    </div>
+
+                    {/* TODO: в отьельный раздел */}
+                    {/* <div className={styles.Description__priceContainer}>
                         <LabelInput label='Цена'>
                             <input
                                 type='number'
@@ -307,8 +522,7 @@ const Description: FC<DescriptionProps> = ({ good }) => {
                                 {...register('discount', { required: false })}
                             />
                         </LabelInput>
-                    </div>
-
+                    </div> */}
                     <input
                         type='submit'
                         value={'Применить изменения'}
