34a5c953d86e082924ed591a9184cf9cbd1e7551
proll4588 <prol4588@gmail.com>
2023-01-18 00:33:15 +0700

    add visit counting

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import ControlLayout from './ControlLayout/ControlLayout'
 import ShopLayout from './ShopLayout/ShopLayout'
 import appTypesAtom, { appTypes } from './atoms/appType.atom'
 import { useRecoilValue } from 'recoil'
+import { useMutation } from '@apollo/client'
+import { ADD_VISIT } from './apollo/fetchs'
 
 const App = () => {
     const appType = useRecoilValue(appTypesAtom)
+    const [visit] = useMutation(ADD_VISIT)
+
+    // useEffect(() => {
+    //     visit()
+    // }, [])
 
     return (
         <div className='App'>
